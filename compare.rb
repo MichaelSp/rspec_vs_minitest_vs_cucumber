1ca81d39cbfe004b28bc0691cedcb0f4c44a1172
MichaelSp <MichaelSp@users.noreply.github.com>
2015-03-12 00:02:43 +0100

    run 100_000 times

--- compare.rb
+++ compare.rb
@@ -1,7 +1,7 @@
 require "benchmark"
 ENV["RAILS_ENV"] = 'test'
 
-times = 10
+times = 100_000
 Benchmark.bm(10) do |x|
   x.report("cucumber:") { (1..times).each { load 'bench_cucumber.rb' } }
   x.report("minitest:") { (1..times).each { load 'bench_minitest.rb' } }
